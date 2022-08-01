d4ca119e887c3bffed3631ce40a5c501fead1c9c
saad6742988 <72674691+saad6742988@users.noreply.github.com>
2022-08-01 15:54:34 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const express=require('express')
+const e=express();
+e.get('/',(req,res)=>
+{
+    res.send("hello expreess")
+})
+e.listen(process.env.PORT||300,()=>{console.log('listening express server')})
