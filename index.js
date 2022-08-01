fb35fecd85c0136c80326fdbdf309902cb34c495
saad6742988 <72674691+saad6742988@users.noreply.github.com>
2022-08-01 16:52:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express')
 const e=express();
 e.get('/',(req,res)=>
 {
+    res.send("hello Saad")
+})
+e.get('/Bilal',(req,res)=>
+{
     res.send("hello bilal")
 })
 e.listen(process.env.PORT||300,()=>{console.log('listening express server')})
