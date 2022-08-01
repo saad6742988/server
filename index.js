118d212fc150f3596dcffac3b65a401d2a1cdc40
saad6742988 <72674691+saad6742988@users.noreply.github.com>
2022-08-01 16:16:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-const http=require('http')
-const port=process.env.Port||3000
-const server=http.createServer((req,res)=>
+const express=require('express')
+const e=express();
+e.get('/',(req,res)=>
 {
-    res.setHeader('Content-Type','text/html');
-    if(req.url==='/')
-    {
-    res.statusCode=200
-    res.end('hello')
-    }
-    else if(req.url==='/About')
-    {
-        res.statusCode=200
-    res.end('about')
-    }
-    else{
-        res.statusCode=404
-    res.end('Not Found')
-    }
+    res.send("hello expreess")
 })
-server.listen(port,()=>{console.log('listening')})
+e.listen(process.env.PORT||300,()=>{console.log('listening express server')})
